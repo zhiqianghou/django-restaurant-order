01d4524fe48ad180e9df8bc09c71cdd76a8f8a9f
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-11-20 16:21:00 -0600

    Initiate the admin interface

--- order_system/admin.py
+++ order_system/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+	list_display = ("meal", "status")
+	list_filter = ("status", ) # This is a tuple, must have a comma
+	search_fields = ("meal", "description")
+
+
+admin.site.register(Item, MenuItemAdmin)
