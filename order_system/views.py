ffa3e8e9780b61c6cd715c98fd2ab7b282bafb8c
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-11-20 16:06:09 -0600

    Add Context function: get_context_data

--- order_system/views.py
+++ order_system/views.py
@@ -6,6 +6,10 @@ class MenuList(generic.ListView):
 	queryset = Item.objects.order_by("-date_created")
 	template_name = "index.html"
 
+	def get_context_data(self):
+		context = {"meals": ["pizza", "salad"]}
+		return context
+
 
 class MenuItemDetail(generic.DetailView):
 	model = Item
